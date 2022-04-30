31a33c0b83970314d113008c501509bc0f351085
Alexsandron3 <alexsandro0602099@outlook.com>
2022-04-30 01:51:24 -0300

    Cria interfaces para Empresas, usuarios e interface generica

--- App/interface/ICrud.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-interface ICrud
-{
-  public function listarTudo();
-  public function listarPorId(int $id);
-  public function cadastrar(array $empresa);
-  public function deletar(int $id);
-}
--- App/interface/IEmpresa.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-interface IEmpresa extends ICrud
-{
-}
