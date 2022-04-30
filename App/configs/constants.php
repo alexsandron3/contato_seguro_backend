177bd3c5e7350bfb604135ebf954c2fa74e3e23e
Alexsandron3 <alexsandro0602099@outlook.com>
2022-04-30 01:52:55 -0300

    Cria arquivo de constantes

---
+++ App/configs/constants.php
@@ -0,0 +1,10 @@
+<?php
+
+// RESPOSTAS DO SERVIDOR
+const SUCESSO_AO_PESQUISAR = 'Pesquisa realizada com sucesso!';
+const NADA_ENCONTRADO_NA_PESQUISA = 'Nenhum registro foi encontrado!';
+
+
+// STATUS HTTP
+const HTTP_STATUS_OK = 200;
+const HTTP_STATUS_NOT_FOUND = 404;
