60d18940f047561f38c220d11af1567b073a8c6a
Alexsandron3 <alexsandro0602099@outlook.com>
2022-04-30 01:51:00 -0300

    Cria conexão local sem implementação de envs

---
+++ App/configs/Database.php
@@ -0,0 +1,20 @@
+<?php
+
+class Database
+{
+  private $host = "localhost";
+  private $database_name = "contato_seguro_desafio";
+  private $username = "root";
+  private $password = "@L3xsandro";
+  public $conn;
+  public function getConnection()
+  {
+    $this->conn = null;
+    try {
+      $this->conn = new \PDO("mysql:host=" . $this->host . ";dbname=" . $this->database_name, $this->username, $this->password);
+    } catch (\PDOException $exception) {
+      echo "Couldnt connect to database: " . $exception->getMessage();
+    }
+    return $this->conn;
+  }
+}
