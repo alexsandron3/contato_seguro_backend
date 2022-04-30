25c8d5a4fab3d56c995413d5a5b059d0de5df875
Alexsandron3 <alexsandro0602099@outlook.com>
2022-04-30 06:09:09 -0300

    Feature: Cria funcao para pegar o ID de uma rota

--- src/app/configs/constants.php
+++ src/app/configs/constants.php
@@ -3,10 +3,19 @@
 // RESPOSTAS DO SERVIDOR
 const SUCESSO_AO_PESQUISAR = 'Pesquisa realizada com sucesso!';
 const SUCESSO_AO_CADASTRAR = 'Cadastro realizado com sucesso!';
+const SUCESSO_AO_DELETAR = 'Registro apagado com sucesso!';
 const NADA_ENCONTRADO_NA_PESQUISA = 'Nenhum registro foi encontrado!';
+const METODO_NAO_PERMITIDO = "Método não permitido!";
+const ID_NAO_INFORMADO = "ID não encontrado na requisição!";
 
 // STATUS HTTP
 const HTTP_STATUS_OK = 200;
 const HTTP_STATUS_CREATED = 201;
 const HTTP_STATUS_BAD_REQUEST = 400;
 const HTTP_STATUS_NOT_FOUND = 404;
+const HTTP_STATUS_METHOD_NOT_ALLOWED = 405;
+
+// TIPOS DE REQUISIÇÃO
+
+const DELETE = "DELETE";
+const GET = "GET";
