2d410e840e4cb9b4b86d7604f8da8b323cc7572e
Alexsandron3 <alexsandro0602099@outlook.com>
2022-04-30 01:52:13 -0300

    Cria model de Empresas

---
+++ App/models/Empresa.php
@@ -0,0 +1,38 @@
+<?php
+
+class Empresa
+{
+  private $conn;
+  private $table = "empresas";
+
+  public $id;
+  public $nome;
+  public $endereco;
+  public $cnpj;
+
+  public function __construct($connection)
+  {
+    $this->conn = $connection;
+  }
+
+  public function listarTudo()
+  {
+    $query = "SELECT * FROM " . $this->table . ";";
+    $stmt = $this->conn->prepare($query);
+    $stmt->execute();
+    return $stmt;
+  }
+
+  public function listarPorId(int $id)
+  {
+    $query = "SELECT * FROM " . $this->table . " WHERE id = :id";
+    $stmt = $this->conn->prepare($query);
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+    $resultado = $stmt->fetch(\PDO::FETCH_ASSOC);
+    $this->id = $resultado['id'];
+    $this->nome = $resultado['nome'];
+    $this->endereco = $resultado['endereco'];
+    $this->cnpj = $resultado['cnpj'];
+  }
+}
