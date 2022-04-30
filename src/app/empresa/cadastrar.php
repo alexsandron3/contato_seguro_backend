9e6016ff6d0c52c8eeb13e92afdc9a3c9fa6fb24
Alexsandron3 <alexsandro0602099@outlook.com>
2022-04-30 04:53:44 -0300

    Feature: Criada rota base para cadastro de nova empresa

---
+++ src/app/empresa/cadastrar.php
@@ -0,0 +1,38 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+use App\Database\Database;
+use App\Empresa;
+
+include_once "../configs/constants.php";
+require_once '../../../vendor/autoload.php';
+
+$database = new Database();
+$conexao = $database->getConnection();
+$empresa = new Empresa($conexao);
+$resposta = array();
+$novaEmpresa = array(
+  "nome" => "Empresa criada pela API",
+  "endereco" => "Lugar nenhum",
+  "cnpj" => "0000000"
+);
+
+$empresaFoiCadastrada = $empresa->cadastrar($novaEmpresa);
+
+if ($empresaFoiCadastrada) {
+  http_response_code(HTTP_STATUS_CREATED);
+  $resposta['mensagem'] = SUCESSO_AO_CADASTRAR;
+  $resposta['empresa'] = array(
+    "id" => $empresa->id,
+    "nome" => $empresa->nome,
+    "endereco" => $empresa->endereco,
+    "cnpj" => $empresa->cnpj,
+  );
+} else {
+  http_response_code(HTTP_STATUS_BAD_REQUEST);
+  $resposta['mensagem'] = SUCESSO_AO_CADASTRAR;
+}
+
+echo json_encode($resposta);
