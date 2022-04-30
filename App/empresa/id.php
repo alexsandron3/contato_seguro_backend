435821138a163be44d3a2c755a336a9da9bd3cbf
Alexsandron3 <alexsandro0602099@outlook.com>
2022-04-30 01:52:30 -0300

    Cria rota para buscar empresa por id

---
+++ App/empresa/id.php
@@ -0,0 +1,33 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once "../configs/constants.php";
+include_once "../bootstrap.php";
+
+$database = new Database();
+
+$connection = $database->getConnection();
+
+$empresa = new Empresa($connection);
+
+$empresa->listarPorId(2);
+$resposta = array();
+
+
+if ($empresa->nome != null) {
+  $resposta['mensagem'] = SUCESSO_AO_PESQUISAR;
+  $resposta['dados'] = array(
+    "id" => $empresa->id,
+    "nome" => $empresa->nome,
+    "cnpj" => $empresa->cnpj,
+    "endereco" => $empresa->endereco,
+  );
+  http_response_code(HTTP_STATUS_OK);
+} else {
+  http_response_code(HTTP_STATUS_NOT_FOUND);
+  $resposta['mensagem'] = NADA_ENCONTRADO_NA_PESQUISA;
+}
+
+echo json_encode($resposta);
